37ff403655c72f3debd046e139f29f38d71b3c1e
Andrew <andrewsylvester117@gmail.com>
2017-08-17 17:09:45 -0600

    Added about us

---
+++ src/components/AboutUs.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+
+class AboutUs extends React.Component {
+    
+    render(){
+        return(
+            <div>
+                <div><p>
+            
+            <br/>Gaming All Time is a Web-App designed to make scoring a Board-Game a much easier expierence.
+            <br/>You have the choice to join games with all your gaming friends and log High Scores and rise to the top.
+            <br/>Gaming All Time was developed by a Team of developers attending Neumont Univeristy in Salt Lake City, Utah. 
+            
+            </p></div>
+            
+            </div>
+        )
+    }
+    
+}
+
+export default AboutUs
