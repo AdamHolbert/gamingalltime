4ff17b0eb014f16ca6e67dc6f485c2e3e836a905
StringerKent <Stringer_K@hotmail.com>
2017-08-17 22:51:37 -0600

    Create buttons to nav to create/select game

---
+++ CreateGame.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { GC_USER_ID, GC_AUTH_TOKEN } from '../constants'
+import { gql, graphql, compose } from 'react-apollo'
+import { Link } from 'react-router-dom'
+
+class CreateGame extends React.Component {
+
+createNewGameClick() {
+        window.location ='/CreateGame_Create'
+    }
+playNewGameClick() {
+        window.location ='/CreateGame_Select'
+    }
+   render(){
+        return(
+            <div>
+                <br></br>
+                <br></br>
+                <p>Creating a new game will add it to your groups list of avaliable games to play.</p>
+                <button onClick={this.createNewGameClick}>Create New Game</button>
+                <br></br>
+                <br></br>
+                <br></br>
+                <p>Select a game from your avaliable list of games</p>
+                <button onClick={this.playNewGameClick}>Start New Game</button>
+            </div>
+        )
+    }
+}
+
+
+export default CreateGame
