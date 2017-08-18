9246b2b8dd52a8ae72fb92ca98d409a5bf514958
Chrstian Moore <CMoore@student.neumont.edu>
2017-08-18 11:51:29 -0600

    People.js is useless

--- src/components/People.js
+++ src/components/People.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react'
-
-class People extends React.Component {
-
-  render() {
-    return (
-        //😎
-      <div>
-        <div>
-        <div>{this.props.link.name} {this.props.link.email} 😎</div>
-        </div>
-      </div>
-    )
-  }
-  
-  
-
-}
-
-
-export default People
+//import React, { Component } from 'react'
+//
+//class People extends React.Component {
+//
+//  render() {
+//    return (
+//        //😎
+//      <div>
+//        <div>
+//        <div>{this.props.link.name} {this.props.link.email} 😎</div>
+//        </div>
+//      </div>
+//    )
+//  }
+//  
+//  
+//
+//}
+//
+//
+//export default People
+//this is useless now
