63ec0dcc8dfa42b51c5cf0ef2e167b37673a1f49
DShoupe <dylanshoupe@yahoo.com>
2017-08-17 23:00:26 -0600

    Fixed a slight merge issue with file structure

--- src/components/CreateGame.js
+++ src/components/CreateGame.js
@@ -4,17 +4,25 @@ import { gql, graphql, compose } from 'react-apollo'
 import { Link } from 'react-router-dom'
 
 class CreateGame extends React.Component {
+
+createNewGameClick() {
+        window.location ='/CreateGame_Create'
+    }
+playNewGameClick() {
+        window.location ='/CreateGame_Select'
+    }
    render(){
         return(
             <div>
                 <br></br>
                 <br></br>
                 <p>Creating a new game will add it to your groups list of avaliable games to play.</p>
-                <button>Create New Game</button>
+                <button onClick={this.createNewGameClick}>Create New Game</button>
                 <br></br>
                 <br></br>
                 <br></br>
-                <button>Start New Game</button>
+                <p>Select a game from your avaliable list of games</p>
+                <button onClick={this.playNewGameClick}>Start New Game</button>
             </div>
         )
     }
