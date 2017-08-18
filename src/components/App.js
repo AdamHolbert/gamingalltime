18b2ce71d055e5c5ade2a28fbee895c420923e6c
AdamHolbert <aholbert@student.neumont.edu>
2017-08-17 22:56:37 -0600

    Added an AccessRestriction on pages hidden by auth

--- src/components/App.js
+++ src/components/App.js
@@ -12,24 +12,33 @@ import HomePage from './HomePage';
 import AboutUsPage from './AboutUsPage';
 import UnknownPage from './UnknownPage';
 import DataScrapeExample from './DataScrapeExample';
+import AccessRestriction from './AccessRestrictionPage';
 
 import { Switch, Route } from 'react-router-dom'
+import { GC_USER_ID, GC_AUTH_TOKEN } from '../constants'
 
 class App extends Component {
     render() {
-      return (
+        const userId = localStorage.getItem(GC_USER_ID)
+        return (
             <div>
                 <Header />
                 <div>
                     <Switch>
+                        {userId ? <Route exact path='/CreateGame' component={CreateGame}/>
+                            : <Route exact path='/CreateGame' component={AccessRestriction}/>}
+                        {userId ? <Route exact path='/CreateGame_Create' component={CreateGame_Create}/>
+                            : <Route exact path='/CreateGame_Create' component={AccessRestriction}/>}
+                        {userId ? <Route exact path='/CreateGame_Select' component={CreateGame_Select}/>
+                            : <Route exact path='/CreateGame_Select' component={AccessRestriction}/>}
+                        {userId ? <Route exact path='/profile' component={ProfilePage}/>
+                            : <Route exact path='/profile' component={AccessRestriction}/>}
+                        {userId ? <Route exact path='/create' component={CreateLink}/>
+                            : <Route exact path='/create' component={AccessRestriction}/>}
+                        {userId ? <Route exact path='/DataScrapeExample' component={DataScrapeExample}/>
+                            : <Route exact path='/DataScrapeExample' component={AccessRestriction}/>}
                         <Route exact path='/AboutUs' component={AboutUsPage}/>
-                        <Route exact path='/CreateGame' component={CreateGame}/>
-                        <Route exact path='/CreateGame_Create' component={CreateGame_Create}/>
-                        <Route exact path='/CreateGame_Select' component={CreateGame_Select}/>
-                        <Route exact path='/profile' component={ProfilePage}/>
                         <Route exact path='/login' component={LoginPage}/>
-                        <Route exact path='/create' component={CreateLink}/>
-                        <Route exact path='/DataScrapeExample' component={DataScrapeExample}/>
                         <Route exact path='/' component={HomePage}/>
                         <Route path='/' component={UnknownPage}/>
                     </Switch>
