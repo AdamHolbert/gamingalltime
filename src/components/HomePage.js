5c62d46b7e9fd94a3f8bf11888a6c968726d7dab
Chrstian Moore <CMoore@student.neumont.edu>
2017-08-25 17:17:33 -0600

    Settled the error

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,9 +7,8 @@ class HomePage extends React.Component {
     render(){
         const userId = localStorage.getItem(GC_USER_ID)
         return(
-<<<<<<< HEAD
             <div>
-                <div><h1>Welcome to the home page!</h1></div>
+                <div className="homeHeader"><h1>Welcome to the home page!</h1></div>
                 {userId ?
                     <div> 
                         <h2>Welcome User!</h2>
@@ -27,27 +26,7 @@ class HomePage extends React.Component {
                     </div>
                 }
             </div>
-=======
-                <div>
-                    <div className="homeHeader"><h1>Welcome to the home page!</h1></div>
-                    {userId ?
-                        <div> 
-                            <h2>Welcome User!</h2>
-                            <h3>All Registered Users</h3>
-                            <PeopleInfo id={userId}/>
-                        </div>
-                            :
-                        <div>
-                        <div>
-                            <h2>Don't Have An Account? Well Create One Today!! </h2>
-                            <h3>All Registered Users</h3>
-                            <LoggedOut/>
-                        </div>
 
-                        </div>
-                    }
-                </div>
->>>>>>> origin/master
         )
     }
 }
