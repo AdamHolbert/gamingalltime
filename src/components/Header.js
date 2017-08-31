cab14048843fbb2e55d3e505cde944a0e1b576b3
AdamHolbert <aholbert@student.neumont.edu>
2017-08-31 17:09:41 -0600

    Put a space between CreateGame

--- src/components/Header.js
+++ src/components/Header.js
@@ -28,7 +28,7 @@ class Header extends Component {
                 <div className="headNavBar">
                     <Link to='/' className="navLink">Home</Link>
                     <Link to='/AboutUs' className="navLink">About Us</Link>
-                    {userId && <Link to='/CreateGame' className="navLink">CreateGame</Link>}
+                    {userId && <Link to='/CreateGame' className="navLink">Create Game</Link>}
 //                    {userId && <Link to='/CreateGame_Create' className="navLink">Create New Game</Link>}
 //                    {userId && <Link to='/CreateGame_Select' className="navLink">Play A Game</Link>}
                     {userId && <Link to='/create' className="navLink">Submit</Link>}
